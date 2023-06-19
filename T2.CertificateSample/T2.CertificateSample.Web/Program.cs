f9bfc04d7cff790ce2ef8d0c90ca7c6273c62a9e
Loai Al-Shakhshir <i.alshakhshir@t2.sa>
2023-06-19 18:44:26 +0300

    Initial Commit

---
+++ T2.CertificateSample/T2.CertificateSample.Web/Program.cs
@@ -0,0 +1,25 @@
+namespace T2.CertificateSample.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ T2.CertificateSample/T2.CertificateSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace T2.CertificateSample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+             var result = await GetApiDataUsingHttpClientHandler();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        private static async Task<string> GetApiDataUsingHttpClientHandler()
+        {
+            var certificatePath = $"{Environment.CurrentDirectory}\\Certificate.pfx";
+            var cert = new X509Certificate2(certificatePath, "123456");
+            var handler = new HttpClientHandler();
+            handler.ClientCertificates.Add(cert);
+            var client = new HttpClient(handler);
+
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri("http://localhost:5149/api/WeatherForecast"),
+                Method = HttpMethod.Get,
+            };
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return responseContent;
+            }
+
+            throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
+        }
+
+    }
+}
